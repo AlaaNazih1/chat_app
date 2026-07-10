620e3b74784aec8b769acda43420a42446e5e5b1
Alaa Nazih Rabie Helmy <Alaanazih87_sd2023@science.bsu.edu.eg>
2026-07-10 18:53:32 +0300

    add login function in login cubit

--- lib/pages/cubits/login_cubit/login_cubit.dart
+++ lib/pages/cubits/login_cubit/login_cubit.dart
@@ -1,8 +1,28 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/widgets.dart';
+import 'package:chat_app/services/auth_service.dart';
 import 'package:meta/meta.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
   LoginCubit() : super(LoginInitial());
+
+  Future<void> login({
+    required BuildContext context,
+    required String email,
+    required String password,
+  }) async {
+    emit(LoginLoading());
+    try {
+      await AuthService.signIn(
+        email: email,
+        password: password,
+        context: context,
+      );
+      emit(LoginSuccess());
+    } catch (e) {
+      emit(LoginFailure());
+    }
+  }
 }
