ab75c99e708ccc1d7ddfd4e6e36225db455334c0
Alaa Nazih Rabie Helmy <Alaanazih87_sd2023@science.bsu.edu.eg>
2026-07-10 18:42:19 +0300

    add login states

---
+++ lib/pages/cubits/login_cubit/login_state.dart
@@ -0,0 +1,9 @@
+part of 'login_cubit.dart';
+
+@immutable
+sealed class LoginState {}
+
+ class LoginInitial extends LoginState {}
+ class LoginLoading extends LoginState {}
+ class LoginSuccess extends LoginState {}
+ class LoginFailure extends LoginState {}
